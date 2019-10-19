f5d03263d12201f0ac79f1140d7579e1b58a6a88
hoannguyenobito <dinhhoannguyen999@gmail.com>
2019-10-19 23:37:02 +0700

    update profile page

---
+++ js/cart.js
@@ -0,0 +1,9 @@
+
+(function(){
+    //add cart
+    var cart = document.getElementsByClassName('single-product-area');
+    if(cart.length > 0) {
+        var cartAddBtns = document.getElementsByClassName('add-to-cart'),
+    
+    }
+})
