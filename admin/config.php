44d4faf3c35aa3f3db7652b865abbe81e5fe9471
ddhien98@gmail.com <ddhien98@gmail.com>
2019-12-13 11:38:12 +0700

    Hien commit admin

---
+++ admin/config.php
@@ -0,0 +1,7 @@
+<?php 
+   $databaseHost = 'localhost';
+   $databaseName = 'web_ass2';
+   $databaseUsername = 'root';
+   $databasePassword = '';
+   $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
